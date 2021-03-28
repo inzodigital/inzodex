731479b9e2aa103a5e8174a9a305b4ab9230cc23
inzo <inzodigital@gmail.com>
2021-03-28 11:59:10 -0400

    title 0_o

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Pokedex" });
+  res.render('index', { title: "inzodex" });
 });
 
 console.log("Currently running on: ", process.platform);
