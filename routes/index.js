1081493a38d976fed17b9fa31574f985dbdd5cb2
inzo <inzodigital@gmail.com>
2021-03-28 16:59:47 -0400

    added mysql dependency

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
-var express = require('express');
+const express = require('express');
+const mysql = require('mysql');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "inzodex" });
 });
+
+// mysql connection
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'holdpls',
+  database: 'inzodex'
+};
+
+// create the pool for mysql db
+const pool = mysql.createPool(config);
 
 console.log("Currently running on: ", process.platform);
 
