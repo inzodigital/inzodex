53db9a7414600f09c535ab1e42b858c39357339b
inzo digital <inzodigital@gmail.com>
2021-04-03 14:25:15 -0400

    db link local

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const mysql = require('mysql');
 
 var app = express();
+
+// mysql connection
+const config = {
+	host: process.env.host,
+	user: process.env.user,
+	password: process.env.password,
+	database: process.env.database
+  };
+  
+// create the pool for mysql db
+const pool = mysql.createPool(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +38,26 @@ app.use('/users', usersRouter);
 
 // set favicon
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+// endpoint 00: select random pokemon name
+app.get('/', function(req, res) {
+	
+	// generate random integer
+	var num = Math.floor(Math.random() * 9) + 1;
+
+	// start query
+	pool.query('SELECT name FROM inzodex WHERE id = ' + num, function(error, rows) {
+		if (error) throw error
+
+		console.log(num);
+
+		// retrieve pokemon name to pass to view
+		var pokemon = rows[0].name;
+
+		// send query results to client
+		res.render('index', {title: 'inzodex', nameResult: pokemon});
+	});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 var router = express.Router();
-require('dotenv').config();
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: "inzodex" });
 });
-
-// mysql connection
-const config = {
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
-};
-
-// create the pool for mysql db
-const pool = mysql.createPool(config);
-
+*/
 console.log("Currently running on: ", process.platform);
 
 module.exports = router;
