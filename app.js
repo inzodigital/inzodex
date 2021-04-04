b25f85e285886a64de4c1781dac72c8e5c91368a
inzo digital <inzodigital@gmail.com>
2021-04-04 11:48:40 -0400

    first

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var favicon = require('serve-favicon');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const sql = require("db.js");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const mysql = require('mysql');
+const app = express();
 
-var app = express();
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
 
-// mysql connection
-const config = {
-	host: process.env.host,
-	user: process.env.user,
-	password: process.env.password,
-	database: process.env.database
-  };
-  
-// create the pool for mysql db
-const pool = mysql.createPool(config);
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// simple route
+app.get("/", (req, res) => {
+	// generate random integer
+	var num = Math.floor(Math.random() * 9) + 1;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+	// start query
+	sql.query('SELECT name FROM inzodex WHERE id = ' + num, function(error, rows) {
+		if (error) throw error
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+		console.log(num);
 
-// set favicon
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+		// retrieve pokemon name to pass to view
+		var pokemon = rows[0].name;
 
-// endpoint 00: select random pokemon name
+		// send query results to client
+		res.render('index', {title: 'inzodex', nameResult: pokemon});
+	});
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+	console.log("Server is running on port 3000.");
+});
+
+
+/* endpoint 00: select random pokemon name
 app.get('/', function(req, res) {
 	
 	// generate random integer
@@ -58,21 +54,4 @@ app.get('/', function(req, res) {
 		res.render('index', {title: 'inzodex', nameResult: pokemon});
 	});
 });
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+*/
